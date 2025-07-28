5b83dd55f0da797745022b54b80f2ff8283ad67e
Pinky Bhargava <pinky.bhargava@in.ibm.com>
2025-07-28 14:00:37 +0530

    updated for CORs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"time"
 	"warehouse-inventory/internal/db"
 	"warehouse-inventory/internal/handler"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,17 @@ func main() {
 	db.Init()
 
 	r := gin.Default()
+
+	// CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://warehouse-inventory-frontend.onrender.com", "http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	api := r.Group("/api")
 	{
 		api.GET("/products", handler.GetProducts)
